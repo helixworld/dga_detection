71570e354818db1d7b934576fb30f73d1442c26b
pgupta <pgupta@pindrop.com>
2019-08-08 09:26:59 -0400

    Added dataset to load

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
 __author__ = "Payas Gupta"
 
+import pandas as pd
 
+
+def load_dataset(fname):
+    """
+    Load the training dataset from a file where fields are separated by ctrl-A
+    :param fname: Filename which contains the dataset
+    :return:
+    """
+    return pd.read_csv(fname, sep='\x01', header=None,
+                       names=['domain', 'origin', 'is_dga'])
